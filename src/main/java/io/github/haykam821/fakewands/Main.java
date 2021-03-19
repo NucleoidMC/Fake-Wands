ae521c9f8789785823ded1ae856aa8aca68c58f8
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-03-19 13:44:27 -0400

    Add the selection wand

--- src/main/java/io/github/haykam821/fakewands/Main.java
+++ src/main/java/io/github/haykam821/fakewands/Main.java
@@ -1,15 +1,26 @@
 package io.github.haykam821.fakewands;
 
+import com.sk89q.worldedit.command.tool.SelectionWand;
+
 import io.github.haykam821.fakewands.item.WandItem;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Main implements ModInitializer {
+	private static final String MOD_ID = "fakewands";
+
+	private static final Identifier SELECTION_WAND_ID = new Identifier(MOD_ID, "selection_wand");
+	public static final WandItem SELECTION_WAND = new WandItem(Items.WOODEN_AXE, new SelectionWand());
+
 	@Override
 	public void onInitialize() {
-		return;
+		Registry.register(Registry.ITEM, SELECTION_WAND_ID, SELECTION_WAND);
 	}
 
 	public static WandItem getWandItem(String id) {
+		if (id.equals(SELECTION_WAND_ID.toString())) return SELECTION_WAND;
 		return null;
 	}
 }
