74ca8af47227744e149ba401ee7a384fc3cb19d5
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-03-19 13:48:30 -0400

    Add the navigation wand

--- src/main/java/io/github/haykam821/fakewands/Main.java
+++ src/main/java/io/github/haykam821/fakewands/Main.java
@@ -1,5 +1,6 @@
 package io.github.haykam821.fakewands;
 
+import com.sk89q.worldedit.command.tool.NavigationWand;
 import com.sk89q.worldedit.command.tool.SelectionWand;
 
 import io.github.haykam821.fakewands.item.WandItem;
@@ -11,15 +12,20 @@ import net.minecraft.util.registry.Registry;
 public class Main implements ModInitializer {
 	private static final String MOD_ID = "fakewands";
 
+	private static final Identifier NAVIGATION_WAND_ID = new Identifier(MOD_ID, "navigation_wand");
+	public static final WandItem NAVIGATION_WAND = new WandItem(Items.COMPASS, new NavigationWand());
+
 	private static final Identifier SELECTION_WAND_ID = new Identifier(MOD_ID, "selection_wand");
 	public static final WandItem SELECTION_WAND = new WandItem(Items.WOODEN_AXE, new SelectionWand());
 
 	@Override
 	public void onInitialize() {
+		Registry.register(Registry.ITEM, NAVIGATION_WAND_ID, NAVIGATION_WAND);
 		Registry.register(Registry.ITEM, SELECTION_WAND_ID, SELECTION_WAND);
 	}
 
 	public static WandItem getWandItem(String id) {
+		if (id.equals(NAVIGATION_WAND_ID.toString())) return NAVIGATION_WAND;
 		if (id.equals(SELECTION_WAND_ID.toString())) return SELECTION_WAND;
 		return null;
 	}
