01c7f8a7c1bf132310adcf5569a8e9ea1f704b18
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-03-19 23:47:14 -0400

    Add the distance wand

--- src/main/java/io/github/haykam821/fakewands/Main.java
+++ src/main/java/io/github/haykam821/fakewands/Main.java
@@ -1,5 +1,6 @@
 package io.github.haykam821.fakewands;
 
+import com.sk89q.worldedit.command.tool.DistanceWand;
 import com.sk89q.worldedit.command.tool.NavigationWand;
 import com.sk89q.worldedit.command.tool.SelectionWand;
 
@@ -12,6 +13,9 @@ import net.minecraft.util.registry.Registry;
 public class Main implements ModInitializer {
 	private static final String MOD_ID = "fakewands";
 
+	private static final Identifier DISTANCE_WAND_ID = new Identifier(MOD_ID, "distance_wand");
+	public static final WandItem DISTANCE_WAND = new WandItem(Items.IRON_AXE, new DistanceWand());
+
 	private static final Identifier NAVIGATION_WAND_ID = new Identifier(MOD_ID, "navigation_wand");
 	public static final WandItem NAVIGATION_WAND = new WandItem(Items.COMPASS, new NavigationWand());
 
@@ -20,11 +24,13 @@ public class Main implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		Registry.register(Registry.ITEM, DISTANCE_WAND_ID, DISTANCE_WAND);
 		Registry.register(Registry.ITEM, NAVIGATION_WAND_ID, NAVIGATION_WAND);
 		Registry.register(Registry.ITEM, SELECTION_WAND_ID, SELECTION_WAND);
 	}
 
 	public static WandItem getWandItem(String id) {
+		if (id.equals(DISTANCE_WAND_ID.toString())) return DISTANCE_WAND;
 		if (id.equals(NAVIGATION_WAND_ID.toString())) return NAVIGATION_WAND;
 		if (id.equals(SELECTION_WAND_ID.toString())) return SELECTION_WAND;
 		return null;
